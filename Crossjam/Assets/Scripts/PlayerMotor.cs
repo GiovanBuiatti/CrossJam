650d2e61f35c8924126ee09705ce704e6be6c734
GiovanBuiatti <92451499+GiovanBuiatti@users.noreply.github.com>
2022-11-06 21:16:03 +0100

    oui

---
+++ Crossjam/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+
+    private float speed = 5.0f;
+
+    private float animationDuration = 2.0f;
+    private float startTime;
+
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isDead)
+        {
+            return;
+        }
+
+
+        if (Time.time - startTime < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+
+        // X - Gauche et Droite
+        moveVector.x = Input.GetAxisRaw("Horizontal")* speed;
+
+        
+        // Z - Avant et Arrière
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+    public void SetSpeed(float modifier)
+    {
+        speed = 5.0f + modifier;
+    }
+
+    //Called everytime our capsule hits something
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.point.z > transform.position.z + controller.radius)
+            Death();
+    }
+    private void Death()
+    {
+        isDead = true;
+        GetComponent<Score>().OnDeath();
+    }
+}
---
+++ Crossjam/Assets/Scripts/TileManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+
+    private Transform playerTransform;
+    //Cette variable sert à faire spawn le sol derriere le joueur au moment du spawn pour que il n'y ait pas du vide
+    private float spawnZ = -6.0f;
+    //Cette variable sert a faire spawn en avance du sol 
+    private float tileLength = 12.0f;
+    //Cette variable est le nombre de tile en jeu 
+    private int amnTilesOnScreen = 7;
+    //Cette variable sert a ce que les tiles ne se suppriment pas trop tot
+    private float safeZone = 15.0f;
+
+    private int lastPrefabIndex = 0;
+    
+    private List<GameObject> activeTiles;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        activeTiles = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+       for(int i = 0; i < amnTilesOnScreen; i++)
+        {
+            if( i < 2)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile();
+            }
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Ce if veut dire : quand on passe un certain point, une nouvelle Tile apparait (principe du niveau infini) / Il sert aussi a supprimer les tiles une fois dépassés
+        if (playerTransform.position.z - safeZone > (spawnZ - amnTilesOnScreen * tileLength))
+        {
+            SpawnTile();
+            DeleteTile();
+        }
+    }
+
+
+    //Ici on a la fonction qui fait spawn des PREFABS ALEATOIREMENT
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        if(prefabIndex == -1)
+        {
+            go = Instantiate(tilePrefabs[RandomPrefabIndex()]) as GameObject;
+        }
+        else
+        {
+            go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+        }
+            go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+        activeTiles.Add(go);
+    }
+
+    //Ici on supprime les prefabs qui sont derriere le joueur 
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    //Cette fonction est celle qui permet le spawn de tile d'être aléatoire
+    private int RandomPrefabIndex()
+    {
+        if(tilePrefabs.Length <= 1)
+        
+        return 0;
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, tilePrefabs.Length);
+        }
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+        
+    }
+}
